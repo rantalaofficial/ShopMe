ae5ab3767703c5c6fb931c6c2d639b5c98c9afb4
MatiasRantala <matias.rantala@hotmail.com>
2021-02-04 14:55:46 +0200

    Switched to use react

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const app = express();
 const port = 3000;
 
-app.use(express.static('public'))
+app.use(express.static('react/shopme/public'))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
