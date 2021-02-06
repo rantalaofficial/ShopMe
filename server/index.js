ca8973a7befb9a83c1fd539864107928c85a6249
MatiasRantala <matias.rantala@hotmail.com>
2021-02-06 23:27:29 +0200

    small fix

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const bodyParser = require("body-parser");
 const path = require('path');
 const express = require('express')
 
+const ShoppingData = require('./ShoppingData.js');
+
 const app = express();
 const port = 8080;
 
@@ -10,21 +12,63 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.listen(port, () => {
-  	console.log(`App listening at http://localhost:${port}`)
+	console.log(`App listening at http://localhost:${port}`)
 });
-
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
-// Handle GET requests to /api route
-app.get("/api", (req, res) => {
-  res.json({ message: "Hello from server!!234324!!! 1234" });
+const shoppingData = new ShoppingData("shoppingData.txt", 10000);
+
+app.get("/api/get_items", (req, res) => {
+	res.json({ listedItems: shoppingData.getListedItems(), itemTypes: shoppingData.getItemTypes() });
 });
 
-// All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("/api/get_item_history", (req, res) => {
+	res.json({ 
+		itemHistory: shoppingData.getItemHistory(), 
+		listsCreated: shoppingData.getListsCreated(), 
+		firstListCreated: shoppingData.getFirstListCreated(),
+		uptime: getUptime(),
+	});
 });
+
+app.get("/api/get_uptime", (req, res) => {
+	res.json(getUptime());
+});
+
+app.get("/api/set_checked/:type", (req, res) => {
+	res.json(shoppingData.checkItem(req.params.type, true));
+});
+
+app.get("/api/set_unchecked/:type", (req, res) => {
+	res.json(shoppingData.checkItem(req.params.type, false));
+});
+
+app.get("/api/delete_listed_items", (req, res) => {
+	shoppingData.deleteListedItems();
+	res.json(true);
+});
+
+app.get("/api/add_item/:type", (req, res) => {
+	res.json(shoppingData.addItem(req.params.type));
+});
+
+//If any other request appears, just return the react app
+app.get('*', (req, res) => {
+	res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
+function getUptime() {
+	uptime = process.uptime();
+	function pad(s) {
+		return (s < 10 ? '0' : '') + s;
+	}
+	let hours = Math.floor(uptime / (60 * 60));
+	let minutes = Math.floor(uptime % (60 * 60) / 60);
+	let seconds = Math.floor(uptime % 60);
+
+	return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+}
 
 
---
+++ server/shoppingData.js
@@ -0,0 +1,126 @@
+const { json } = require('express');
+const fs = require('fs');
+
+class ShoppingData {
+
+    constructor(filename, savingTimeout) {
+        /*Default data*/
+        this.itemTypes = [];
+        this.listedItems = {};
+        this.itemHistory = {};
+        this.listsCreated = 0;
+        this.firstListCreated = null;
+
+        this.filename = filename;
+        this.savingTimeout = savingTimeout;
+        this.savingTimeoutSet = false;
+
+        if(fs.existsSync(filename)) {
+            let data = JSON.parse(fs.readFileSync(filename, 'utf8'));
+
+            this.itemTypes = data.itemTypes;
+            this.listedItems = data.listedItems;
+            this.itemHistory = data.itemHistory;
+            this.listsCreated = data.listsCreated;
+            this.firstListCreated = data.firstListCreated;
+        }
+    }
+
+    addItem(type) {
+        if (!type) return false; 
+
+        type = type.toLowerCase();
+
+        //If item already exists, return false
+        if(this.listedItems[type] !== undefined) {
+            return false;
+        }
+
+        this.listedItems[type] = false;
+
+        //Creates new item type if doesn't already exist
+        if (!this.itemTypes.includes(type)) {
+            this.itemTypes.push(type);
+        }
+
+        this.saveToFile();
+        return true;
+    }
+
+    checkItem(type, checked) {
+        if (!type) return false; 
+
+        type = type.toLowerCase();
+
+        if (this.itemTypes.includes(type)) {
+            this.listedItems[type] = checked;
+            this.saveToFile();
+            return true;
+        }
+        return false;
+    }
+
+    deleteListedItems() {
+        Object.keys(this.listedItems).map((label, index) => {
+            //Add items to history
+            if(this.listedItems[label]) {
+                if(this.itemHistory[label] === undefined) {
+                    this.itemHistory[label] = 1;
+                } else {
+                    this.itemHistory[label] += 1;
+                }
+            }
+        });
+        this.listsCreated += 1;
+        this.listedItems = {};
+
+        if(this.firstListCreated === null) {
+            let d = new Date()
+            this.firstListCreated = d.getDate() + "." + (d.getMonth() + 1) + "." + d.getFullYear();
+        }
+
+        this.saveToFile();
+    }
+
+    saveToFile() {
+        if(!this.savingTimeoutSet) {
+            this.savingTimeoutSet = true;
+
+            let data = JSON.stringify({
+                itemTypes: this.itemTypes,
+                listedItems: this.listedItems,
+                itemHistory: this.itemHistory,
+                listsCreated: this.listsCreated,
+                firstListCreated: this.firstListCreated,
+            });
+
+            setTimeout(() => {
+                fs.writeFile(this.filename, data, 'utf8', () => {/*console.log("Saved")*/});
+                this.savingTimeoutSet = false;
+            }, this.savingTimeout);
+        }
+    }
+
+    //Getters
+    getListedItems() {
+        return this.listedItems;
+    }
+
+    getItemHistory() {
+        return this.itemHistory;
+    }
+
+    getListsCreated() {
+        return this.listsCreated;
+    }
+
+    getFirstListCreated() {
+        return this.firstListCreated;
+    }
+
+    getItemTypes() {
+        return this.itemTypes;
+    }
+}
+
+module.exports = ShoppingData;
